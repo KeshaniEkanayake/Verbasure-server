3ba932e1e0c8093904d9bee527fb61dc59661837
saiNan24 <saitharsan@gmail.com>
2023-11-16 11:36:51 +0530

    server initial commit

---
+++ src/test/java/com/example/verbasure/VerbasureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verbasure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbasureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
