0a207553a95892b07384e0e82caafc2f7e8ca4fb
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-11-17 20:07:30 +0530

    Testing responses

--- src/main/java/com/example/verbasure/controllers/QuestionQnAController.java
+++ src/main/java/com/example/verbasure/controllers/QuestionQnAController.java
@@ -1,7 +1,14 @@
 package com.example.verbasure.controllers;
 
+import com.example.verbasure.models.Challenge;
+import com.example.verbasure.models.Question;
 import com.example.verbasure.models.QuestionQnA;
+import com.example.verbasure.models.Task;
+import com.example.verbasure.repositories.ChallengeRepository;
 import com.example.verbasure.repositories.QuestionQnARepository;
+import com.example.verbasure.repositories.QuestionRepository;
+import com.example.verbasure.repositories.TaskRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +17,29 @@ import java.util.List;
 @RestController
 public class QuestionQnAController {
 
-    public QuestionQnAController(QuestionQnARepository repository) {
-        this.repository = repository;
+    public final ChallengeRepository challengeRepository;
+    public final TaskRepository taskRepository;
+    public final QuestionRepository questionRepository;
+
+    public QuestionQnAController(ChallengeRepository challengeRepository, TaskRepository taskRepository,
+            QuestionRepository questionRepository) {
+        this.challengeRepository = challengeRepository;
+        this.taskRepository = taskRepository;
+        this.questionRepository = questionRepository;
     }
 
-    private final QuestionQnARepository repository;
-
-    @GetMapping("/qnaquestions")
-    public List<QuestionQnA> getQnAQuestions() {
-        return repository.findAll();
+    @GetMapping("/reading/1")
+    public Challenge getReading1Content() {
+        //find the first challenges content
+        Challenge challenge = challengeRepository.findById(1).get();
+        challenge.getTasks();
+        return challenge;
     }
 
+    @GetMapping("/reading/1/task")
+    public Task getReading1Questions() {
+        Task task = taskRepository.findById(1).get();
+        task.getQuestions();
+        return task;
+    }
 }
