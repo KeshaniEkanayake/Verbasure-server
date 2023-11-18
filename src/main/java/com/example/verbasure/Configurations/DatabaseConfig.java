19d5e29d3be9826110aa2af5e2d41d1891f8b7f4
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-11-18 18:46:14 +0530

    Set data

--- src/main/java/com/example/verbasure/Configurations/DatabaseConfig.java
+++ src/main/java/com/example/verbasure/Configurations/DatabaseConfig.java
@@ -17,77 +17,152 @@ class LoadDatabase {
             QuestionRepository questionRepository) {
 
         return args -> {
+            Challenge challenge;
+            Task task;
+            Question question;
 
             // Reading challenge 1
-            Challenge challenge1 = new Challenge();
-            challenge1.setChallengeTitle("Time to Relax");
-            challenge1.setChallengeDescription(
+            challenge = new Challenge();
+            challenge.setChallengeTitle("Time to Relax");
+            challenge.setChallengeDescription(
                     "Come and join our lunchtime yoga class with experienced yoga teacher Divya Bridge!\n" +
                             "When? Every Tuesday at 1.30 p.m.\n" +
                             "Where? Meeting Room 7\n" +
                             "How much? £10 for four 30-minute classes.\n" +
                             "What to bring? Comfortable clothes. Divya will provide the yoga mats.\n" +
                             "How to join? Write to Sam at");
-            challenge1.setChallengeImage("reading1.jpg");
-            challenge1.setModule(1);
-            challenge1 = challengeRepository.save(challenge1);
+            challenge.setChallengeImage("reading1.jpg");
+            challenge.setModule(1);
+            challenge = challengeRepository.save(challenge);
 
             // Tasks related to challenge 1
 
             // Task 1
-            Task task1 = new Task();
-            task1.setChallenge(challenge1);
-            task1.setQuestionType("questionAndAnswer");
-            task1 = taskRepository.save(task1);
+            task = new Task();
+            task.setChallenge(challenge);
+            task.setQuestionType("questionAndAnswer");
+            task = taskRepository.save(task);
 
-            Question question;
             // Questions related to task 1 - Question and Answer
-            question= new Question("Which day of the week does the Yoga class take place?", "Tuesday");
-            question.setTask(task1);
+            question = new Question("Which day of the week does the Yoga class take place?", "Tuesday");
+            question.setTask(task);
             questionRepository.save(question);
 
-            question= new Question("How many classes can you take for £10?", "Four");
-            question.setTask(task1);
+            question = new Question("How many classes can you take for £10?", "Four");
+            question.setTask(task);
             questionRepository.save(question);
 
             question = new Question("What time will the class end?", "2 p.m.");
-            question.setTask(task1);
+            question.setTask(task);
             questionRepository.save(question);
 
             question = new Question("What kind of clothes do you need to where to the class?",
                     "Comfortable");
-            question.setTask(task1);
+            question.setTask(task);
             questionRepository.save(question);
 
             question = new Question("What do you need to send to Sam if you want to join?", "an email");
-            question.setTask(task1);
+            question.setTask(task);
             questionRepository.save(question);
 
             // Task 2
-            Task task2 = new Task();
-            task2.setChallenge(challenge1);
-            task2.setQuestionType("trueOrFalse");
-            task2 = taskRepository.save(task2);
+            task = new Task();
+            task.setChallenge(challenge);
+            task.setQuestionType("trueOrFalse");
+            task = taskRepository.save(task);
             // Questions related to task 2 - True or False
             question = new Question("Sam Holden is the Yoga teacher", "True");
-            question.setTask(task2);
+            question.setTask(task);
             questionRepository.save(question);
 
             question = new Question("The Yoga class is once a week", "True");
-            question.setTask(task2);
+            question.setTask(task);
             questionRepository.save(question);
 
             question = new Question("The class is at lunch time in room 7", "True");
-            question.setTask(task2);
+            question.setTask(task);
             questionRepository.save(question);
 
             question = new Question("You need to bring yoga mats to the class", "False");
-            question.setTask(task2);
+            question.setTask(task);
             questionRepository.save(question);
 
             question = new Question("The class cannot take 22 people", "False");
-            question.setTask(task2);
+            question.setTask(task);
             questionRepository.save(question);
+
+            // Task 3 - multiple choice
+            task = new Task();
+            task.setChallenge(challenge);
+            task.setQuestionType("multipleChoice");
+            task = taskRepository.save(task);
+            // Questions related to task 3 - Multiple Choice
+            question = new Question("What is the class called?", "Yoga");
+            question.setTask(task);
+            question.setAnswerOptions(new String[] { "Yoga", "Relax", "Lunchtime", "Meeting" });
+            questionRepository.save(question);
+
+            question = new Question("What time does the class start?", "1.30 p.m.");
+            question.setTask(task);
+            question.setAnswerOptions(new String[] { "1.30 p.m.", "1 p.m.", "2 p.m.", "12.30 p.m." });
+            questionRepository.save(question);
+            
+
+            // Reading challenge 2
+            /*
+             * readingText2-departure.7785dd5e.jpg
+             * An Airport Departures Board
+             * Time Flight Destination Gate Remarks
+             * 10:45 XZ 3265 Chennai 17 Departed
+             * 10:50 SQ 0972 Amsterdam 23 Departed
+             * 11:00 SQ 2509 Mexico City 19 Departed
+             * 11:05 RT 9122 Buenos Aires 34 Cancelled
+             * 11:10 XZ 6005 Los Angeles 05 Final call
+             * 11:20 QE 0078 Beijing 33 Final call
+             * 11:25 TY 2053 Christchurch 56 Delayed
+             * 11:25 ER 7452 Seoul 25 Boarding
+             * 11:30 XZ 0352 Moscow 12 Boarding
+             * 11:35 XZ 0569 Kuala Lumpur 27 Gate change
+             */
+            challenge = new Challenge();
+            challenge.setChallengeTitle("Departures");
+            challenge.setChallengeDescription(
+                    "An Airport Departures Board\n" +
+                            "Time\tFlight\tDestination\tGate\tRemarks\n" +
+                            "10:45\tXZ 3265\tChennai\t17\tDeparted\n" +
+                            "10:50\tSQ 0972\tAmsterdam\t23\tDeparted\n" +
+                            "11:00\tSQ 2509\tMexico City\t19\tDeparted\n" +
+                            "11:05\tRT 9122\tBuenos Aires\t34\tCancelled\n" +
+                            "11:10\tXZ 6005\tLos Angeles\t05\tFinal call\n" +
+                            "11:20\tQE 0078\tBeijing\t33\tFinal call\n" +
+                            "11:25\tTY 2053\tChristchurch\t56\tDelayed\n" +
+                            "11:25\tER 7452\tSeoul\t25\tBoarding\n" +
+                            "11:30\tXZ 0352\tMoscow\t12\tBoarding\n" +
+                            "11:35\tXZ 0569\tKuala Lumpur\t27\tGate change\n");
+            challenge.setChallengeImage("readingText2-departure.jpg");
+            challenge.setModule(1);
+            challenge = challengeRepository.save(challenge);
+
+            // Tasks related to challenge 2
+
+            // Task 1
+            /*
+             * The plane isn't here now.
+             * 
+             * The plane is late.
+             * 
+             * The plane is not going to leave.
+             * 
+             * You can get on the plane now.
+             * 
+             * The plane is leaving very soon.
+             * 
+             * You need to go to a different place to get on the plane.
+             * 
+             * 
+             */
+            Task task3 = new Task();
+
         };
     }
 }
---
+++ src/main/java/com/example/verbasure/controllers/ChallengeController.java
@@ -0,0 +1,57 @@
+package com.example.verbasure.controllers;
+
+import com.example.verbasure.DTO.ChallengeDTO;
+import com.example.verbasure.models.Challenge;
+import com.example.verbasure.models.Task;
+import com.example.verbasure.repositories.ChallengeRepository;
+import com.example.verbasure.repositories.QuestionRepository;
+import com.example.verbasure.repositories.TaskRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ChallengeController {
+
+    public final ChallengeRepository challengeRepository;
+    public final TaskRepository taskRepository;
+    public final QuestionRepository questionRepository;
+
+    public ChallengeController(ChallengeRepository challengeRepository, TaskRepository taskRepository,
+            QuestionRepository questionRepository) {
+        this.challengeRepository = challengeRepository;
+        this.taskRepository = taskRepository;
+        this.questionRepository = questionRepository;
+    }
+
+    //get all challenges for reading
+    @GetMapping("/reading")
+    public Map<String, List<?>> getReading1Content() {
+        List<Challenge> challenges = challengeRepository.findByModule(1);
+        // List<ChallengeIdDto> challengeIdDtos = ChallengeIdDto.createChallengeIdDtoList(challenges);
+        List<Integer> challengeIds = challengeRepository.findIdByModule(1);
+        Map<String, List<?>> map = new HashMap<>();
+        map.put("challengeIds", challengeIds);
+        return map;
+    }
+
+    //get challenge by id
+    @GetMapping("/challenge/{id}")
+    public ChallengeDTO getChallengeById(@PathVariable int id) {
+        Challenge challenge = challengeRepository.findById(id).get();
+        ChallengeDTO challengeDTO = ChallengeDTO.challengeDTOFromChallenge(challenge);
+        return challengeDTO;
+    }
+
+    @GetMapping("/reading/1/task")
+    public Task getReading1Questions() {
+        Task task = taskRepository.findById(1).get();
+        task.getQuestions();
+        return task;
+    }
+}
