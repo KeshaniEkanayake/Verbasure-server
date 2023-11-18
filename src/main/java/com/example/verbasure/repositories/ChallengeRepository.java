936a8862aed74c563fd3d7eb3f6e970db4d590c6
Luqman Fazal <2020is047@stu.ucsc.cmb.ac.lk>
2023-11-18 18:40:08 +0530

    Added a method to find by module

--- src/main/java/com/example/verbasure/repositories/ChallengeRepository.java
+++ src/main/java/com/example/verbasure/repositories/ChallengeRepository.java
@@ -1,8 +1,15 @@
 package com.example.verbasure.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.example.verbasure.models.Challenge;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Integer> {
-    
+    List<Challenge> findByModule(int moduleId);
+    @Query("SELECT c.challengeId FROM Challenge c WHERE c.module = ?1")
+    List<Integer> findIdByModule(@Param("1") int moduleId);
 }
