ee0292cebf19f9a019efd52203e557ba07d0d78c
saiNan24 <saitharsan@gmail.com>
2023-11-18 18:50:59 +0530

    Update QuestionQnAController.java

--- src/main/java/com/example/verbasure/controllers/QuestionQnAController.java
+++ src/main/java/com/example/verbasure/controllers/QuestionQnAController.java
@@ -9,11 +9,14 @@ import com.example.verbasure.repositories.QuestionQnARepository;
 import com.example.verbasure.repositories.QuestionRepository;
 import com.example.verbasure.repositories.TaskRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class QuestionQnAController {
 
